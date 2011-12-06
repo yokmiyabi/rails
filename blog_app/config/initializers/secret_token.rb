da7fc8120f376acf92920f0e21cefe01cc2fcb9d
Yoshio Okumura <miyabi.okumura@gmail.com>
2011-12-06 18:54:36 +0900

    add blog_app prj

---
+++ blog_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlogApp::Application.config.secret_token = '79b4b6e97a51d15da4ad3080845d7d92f99217960bfcaa56f7dcb18f7dd7c298cf745ca60c2e4d6a2b51d90b64e92d2f1b2d6d0439322d904b130e7a38f82869'
