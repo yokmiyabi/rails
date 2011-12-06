894b9292a9afbf8a2955863617596fa9d2b7f651
Yoshio Okumura <yoshio@yoshio-ubuntu.yoshio.com>
2011-12-06 17:42:59 +0900

    first commit for training rails

---
+++ railbook/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railbook::Application.config.secret_token = 'fd94d3e6e5162f6b26d41f292a07da9ac38e5a4c287a40bde9ad2ba1266837f4101d7917b910969d6cc7a3b11d25c2008717c66f370395f208a38a40952e436a'
